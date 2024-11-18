18cbc8d9629182f2deb5dc0d2aaba45407fe34ec
Raja Asokan Charuvil <raja.asokan.charuvil@dbschenker.com>
2024-11-18 11:05:59 +0100

    finished ReadDataSource

--- src/main/scala/modernflink/section1/ReadDataSource.scala
+++ src/main/scala/modernflink/section1/ReadDataSource.scala
@@ -12,36 +12,36 @@ import java.time.Instant
 @main def readDataSource() =
   val env = StreamExecutionEnvironment.getExecutionEnvironment
 
-//  // 1. read from a collection
+  // 1. read from a collection
 //  val testStreamOne = env.fromCollection(Seq(
 //    HumidityReading("Flagstaff", 1686122515, 58),
-//    HumidityReading("Flagstaff", 1686208915, 59)
+//    HumidityReading("Flagstaff", 1686122915, 59)
 //  ))
 //
 //  testStreamOne.print("OutputStream1").setParallelism(2)
 //
 //  env.execute()
 
-  // 2. stream from a file
-  val testStream2 = env
-    .readTextFile("src/main/resources/Humidity.txt")
-    .map(HumidityReading.fromString)
-
-  testStream2.print()
-  env.execute()
-
-//   // 3. stream from a socket
-//  val testStreamThree = env.socketTextStream("127.0.0.1", 1235)
-////  testStreamThree.print()
-////  env.execute()
+  // 2. read from a File
+//  val testStream2 = env
+//    .readTextFile("src/main/resources/Humidity.txt")
+//    .map(HumidityReading.fromString)
 //
-//  // 4. read from a data generator
-//  given instantTypeInfo: TypeInformation[Instant] = BasicTypeInfo.INSTANT_TYPE_INFO
-//  val genEvents = SubscriptionEventsGenerator(
-//    sleepSeconds = 1,
-//    startTime = Instant.parse("2023-08-13T00:00:00.00Z")
-//  )
-//
-//  val testStreamFour: DataStream[SubscriptionEvent] = env.addSource(genEvents)
-//  testStreamFour.print()
+//  testStream2.print()
 //  env.execute()
+
+   // 3. stream from a socket
+//  val testStreamThree = env.socketTextStream("127.0.0.1", 1235)
+//  testStreamThree.print()
+//  env.execute()
+
+  // 4. read from a data generator
+  given instantTypeInfo: TypeInformation[Instant] = BasicTypeInfo.INSTANT_TYPE_INFO
+  val genEvents = SubscriptionEventsGenerator(
+    sleepSeconds = 1,
+    startTime = Instant.parse("2023-08-13T00:00:00.00Z")
+  )
+
+  val testStreamFour: DataStream[SubscriptionEvent] = env.addSource(genEvents)
+  testStreamFour.print()
+  env.execute()
